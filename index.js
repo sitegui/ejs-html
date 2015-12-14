58c0770b664a4be9f2acf4744c0e6f201fbed910
Guilherme Souza <sitegui@sitegui.com.br>
2015-12-14 19:04:34 -0200

    Add compile function

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict'
+
+module.exports.compile = require('./lib/compile')
+
+module.exports.parse = require('./lib/parse')
+module.exports.reduce = require('./lib/reduce')
+module.exports.escape = require('./lib/escape')
