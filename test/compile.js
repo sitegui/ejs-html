2bff5327ac8e7dccb4fa80f5c3abf23e1e8699a1
Guilherme Souza <sitegui@sitegui.com.br>
2015-12-15 16:55:45 -0200

    Add test for compile

---
+++ test/compile.js
@@ -0,0 +1,27 @@
+/*globals describe, it*/
+'use strict'
+
+let compile = require('../lib/compile')
+require('should')
+
+describe('parse', function () {
+	it('should compile to run in the server', function () {
+		compile('Hi <b><%=name.first%></b> <%=name.last%>!')({
+			name: {
+				first: 'Gui',
+				last: 'S'
+			}
+		}).should.be.equal('Hi <b>Gui</b> S!')
+	})
+
+	it('should compile to run in the client', function () {
+		compile('Hi <b><%=name.first%></b> <%=name.last%>!', {
+			standAlone: true
+		})({
+			name: {
+				first: 'Gui',
+				last: 'S'
+			}
+		}).should.be.equal('Hi <b>Gui</b> S!')
+	})
+})
